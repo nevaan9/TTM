5d41851bd932407daceb66685956d8cdf59581f6
Nevaan Perera <nevaan9@gmail.com>
2019-01-16 16:41:25 -0500

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+// Import project dependancies
+express = require('express');
+bodyParser = require('body-parser');
+cors = require('cors');
+app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Handle Production
+// Point to which public folder is our entry point
+if (process.env.NODE_ENV === 'production') {
+    // Use the public folder created via npm run build
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle Single Page Applications
+    app.use(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html');
+    });
+}
+
+// Start server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
