0f9f6dfde5bdaf114e72c349943e9f0b435f3e2e
Nevaan Perera <nevaan9@gmail.com>
2019-01-16 16:40:33 -0500

    Init commit

---
+++ client/src/components/Photos.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>Photos</h2>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                drawer: null,
+                items: [
+                    { title: 'Home', icon: 'dashboard' },
+                    { title: 'About', icon: 'question_answer' }
+                ],
+                right: null
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
