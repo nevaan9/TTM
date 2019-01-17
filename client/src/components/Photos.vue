b6972d0b8a1cb23730a56a53a318762d787b9d1a
Nevaan Perera <nevaan9@gmail.com>
2019-01-17 18:52:03 -0500

    Added a photo scroller

--- client/src/components/Photos.vue
+++ client/src/components/Photos.vue
@@ -4,17 +4,18 @@
     >
         <!--ALBUMS NAV DRAWER-->
         <v-btn
+                class="secondary"
                 small
                 block
                 @click.stop="drawer = !drawer"
-                style="margin-left: 40%; margin-right: 40%"
+                style="margin: 0;"
         >
-            Albums
+            <v-icon>home</v-icon>
         </v-btn>
         <v-navigation-drawer
                 v-model="drawer"
+                small
                 absolute
-                dark
                 temporary
         >
             <v-list class="pt-0" dense>
@@ -36,20 +37,16 @@
         </v-navigation-drawer>
 
         <!--PHOTOS-->
-        <v-container fluid grid-list-sm style="padding-top: 0px">
+        <v-container fluid grid-list-lg style="padding-top: 0px">
             <v-layout
                     row
                     wrap
             >
                 <v-flex
-                        v-for="n in 10"
-                        :key="n"
-                        xs6
-                        sm4
+                        class="grid"
                 >
-                    <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg">
-                        <div class="fill-height bottom-gradient"></div>
-                    </v-img>
+                    <img class="img" v-for="(image, i) in images" :src="image" :key="i" @click="index = i">
+                    <vue-gallery-slideshow :images="images" :index="index" @close="index = null"></vue-gallery-slideshow>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -57,20 +54,47 @@
 </template>
 
 <script>
+    import VueGallerySlideshow from 'vue-gallery-slideshow';
+    import image1 from '../images/captain.jpeg'
+    import image2 from '../images/snow.jpeg'
     export default {
+        name: 'Photos',
+        components: {
+            VueGallerySlideshow
+        },
         data () {
             return {
                 drawer: null,
                 items: [
-                    { title: 'Home', icon: 'dashboard' },
-                    { title: 'About', icon: 'question_answer' }
+                    { title: 'All Photos', icon: 'dashboard' },
+                    { title: 'SLU', icon: 'question_answer' },
+                    { title: 'China', icon: 'question_answer' },
                 ],
-                right: null
+                right: null,
+                images: [
+                    image2,
+                    'https://placekitten.com/801/800',
+                    'https://placekitten.com/802/800',
+                    'https://placekitten.com/803/800',
+                    'https://placekitten.com/804/800',
+                    'https://placekitten.com/805/800',
+                    image1
+                ],
+                index: null
             }
         }
     }
 </script>
 
 <style>
-
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        grid-gap: 20px;
+        align-items: center;
+    }
+    .grid img {
+        box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+        max-width: 100%;
+    }
 </style>
